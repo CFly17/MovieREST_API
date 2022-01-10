525fb01251ba39ff46d6b250f6e5ce89127b0126
Cullin Flynn <cullinflynn@rocketmortgage.com>
2022-01-09 22:41:19 -0500

    adding 1-4 with some of 5 done

---
+++ MovieREST_API_Lab/Controllers/MovieController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieREST_API_Lab.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        MovieDAL md = new MovieDAL();
+
+        [HttpGet]
+        public List<Movie> GetMovies()
+        {
+            return md.GetAllMovies();
+        }
+
+        [HttpGet("genre={value}")]
+        public List<Movie> GetMoviesByGenre(string value)
+        {
+            return md.GetMoviesByGenre(value);
+        }
+
+        [HttpGet("random")]
+        public Movie GetRandomMovie()
+        {
+            return md.GetRandomMovie();
+        }
+
+        [HttpGet("genre={value}/random")]
+        public Movie GetRandomByGenre(string value)
+        {
+            return md.GetRandomByGenre(value);
+        }
+
+        [HttpGet("Moviesbynumber/number={value}")]
+        public List<Movie> MoviesByNumber(int value)
+        {
+            return md.GetMoviesByAmount(value);
+        }
+
+    }
+}
---
+++ MovieREST_API_Lab/MovieDAL.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieREST_API_Lab
+{
+    public class MovieDAL
+    {
+        public List<Movie> GetAllMovies()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "select * from movies";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+                return movies;
+            }
+        }
+
+        public List<Movie> GetMoviesByGenre(string genre)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where genre = '{genre}'";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+
+                if (movies.Count >= 1)
+                {
+                    return movies;
+                }
+                else
+                {
+                    List<Movie> error = new List<Movie>();
+                    error.Add(new Movie());
+                    error[0].Title = $"No movie found of the genre '{genre}.'";
+                    return error;
+                }
+            }
+        }
+
+        public Movie GetRandomMovie()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "select * from movies";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+
+                //This creates a random number based on the count of our movie list.
+                //This random number then becomes the index by which we get a random movie.
+                Random rnd = new Random();
+                int randomNum = rnd.Next(0, movies.Count);
+                connect.Close();
+
+                return movies[randomNum];
+            }
+        }
+
+        public Movie GetRandomByGenre(string genre)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where genre = '{genre}'";
+                try
+                {
+                    connect.Open();
+                    List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                    Random rnd = new Random();
+                    int randomNum = rnd.Next(0, movies.Count);
+                    connect.Close();
+                    return movies[randomNum];
+                }
+                catch (InvalidOperationException)
+                {
+                    Movie error = new Movie();
+                    error.Title = $"No movie found of the genre '{genre}.'";
+                    return error;
+                }
+            }
+        }
+
+        public List<Movie> GetMoviesByAmount(int num)
+        {
+            List<Movie> movies = GetAllMovies();
+            List<Movie> randomMovies = new List<Movie>();
+
+            for (int i = 0; i < num; i++)
+            {
+                Random rnd = new Random();
+                int randomNum = rnd.Next(0, movies.Count);
+                randomMovies.Add(movies[randomNum]);
+            }
+
+            return randomMovies;
+        }
+    }
+
+}
